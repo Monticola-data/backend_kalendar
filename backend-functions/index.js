1439d9993674847d63769dbf48b291f24bb867ee
Monticola-data <bursova@monticola-data.cz>
2025-03-18 15:54:12 +0100

    Update index.js

--- backend-functions/index.js
+++ backend-functions/index.js
@@ -395,7 +395,7 @@ exports.updateFirestoreEvent = onRequest(async (req, res) => {
         color: partyColor,
         extendedProps: {
             detail,
-            cas,
+            cas: cas ? Number(cas) : 0,
             hotove: hotove === true || hotove === "true",
             predane: predane === true || predane === "true",
             odeslane: odeslane === true || odeslane === "true",
